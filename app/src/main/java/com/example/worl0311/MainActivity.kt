a28d4971e2211e2bc6539bfbca832dda78dad94a
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 11:36:46 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -2,41 +2,40 @@ package com.example.worl0311
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val resources = LocalContext.current.resources
-            Column {
-                Text(
-                    text = stringResource(R.string.app_name),
-                    fontSize = 28.sp
-                )
-                Text(
-                    text = stringResource(R.string.welcome),
-                    fontSize = 28.sp
-                )
-                Button(onClick={}){
-                    Text(
-                        text = stringResource(R.string.click_button),
-                        fontSize = 22.sp
+            val checkedState = remember { mutableStateOf(false) }
+            val textColor = remember { mutableStateOf(Color.Unspecified) }
+            Column(modifier = Modifier.fillMaxSize().padding(10.dp)){
+                Row (verticalAlignment = Alignment.CenterVertically ){
+                    Text("Красный цвет", fontSize = 22.sp, color = textColor.value)
+                    Switch(
+                        checked = checkedState.value,
+                        onCheckedChange = {
+                            checkedState.value = it
+                            if(checkedState.value) textColor.value = Color(0xFFC62828)
+                            else textColor.value = Color.Unspecified
+                        },
+                        colors = SwitchDefaults.colors(
+                            checkedThumbColor = Color(0xFFC62828),
+                            checkedTrackColor = Color(0xFFE57373),
+                            uncheckedThumbColor = Color(0xFFEF9A9A),
+                            uncheckedTrackColor = Color(0xFFFFEBEE)
+                        )
                     )
                 }
-                Text(
-                    text = stringResource(R.string.user_data, "Tom", 37, "JetBrains"),
-                    fontSize = 28.sp
-                )
-                Text(
-                    text = stringResource(R.string.user_data, "Bob", 41, "Google"),
-                    fontSize = 28.sp
-                )
             }
         }
     }
