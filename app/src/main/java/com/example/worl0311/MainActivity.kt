240148f59ffe8ba064b8c63da62c35979cfbbda8
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 11:43:39 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -1,86 +1,98 @@
 package com.example.worl0311
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.DrawerState
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.*
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
-    @SuppressLint("RememberReturnType", "UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldState = remember {(SnackbarHostState())}
+            var progress by remember { mutableStateOf(0.0f) }
             val scope = rememberCoroutineScope()
-            val selectedItem = remember{ mutableStateOf("")}
-            val items = listOf("Главная", "Контакты", "О приложении")
-            val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-            ModalNavigationDrawer(
-                drawerState = drawerState,
-                drawerContent = {
-                    ModalDrawerSheet {
-                        for (item in items) {
-                            Text(
-                                item,
-                                fontSize = 28.sp,
-                                modifier = Modifier.clickable {
-                                    selectedItem.value = item
-                                    scope.launch { drawerState.close() }
-                                }
-                            )
-                        }
-                    }
-                },
-            ) {
-                Scaffold(
-                    floatingActionButton = {
-                        ExtendedFloatingActionButton(
-                            text = { Text("Show drawer") },
-                            icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-                            onClick = {
-                                scope.launch {
-                                    drawerState.apply {
-                                        if (isClosed) open() else close()
-                                    }
-                                }
+
+            Column{
+                Text("Статус: $progress", fontSize = 22.sp)
+                OutlinedButton(
+                    onClick = {
+                        scope.launch {
+                            while (progress < 1f) {
+                                progress += 0.1f
+                                delay(1000L)
                             }
-                        )
-                    }
-                ){
-                    Column{
-                        Button(onClick = {
-                            scope.launch{ drawerState.open() }
-                        }) {
-                            Text("Меню", fontSize = 28.sp)
                         }
-                        Text("Выбран пункт: ${selectedItem.value}", fontSize = 28.sp)
+                    }
+                ) {
+                    Text("Запустить", fontSize = 22.sp)
+                }
+                CircularProgressIndicator(progress = progress)
+            }
+            onClick = {
+                scope.launch {
+                    while (progress < 1f) {
+                        progress += 0.1f
+                        delay(1000L)
                     }
                 }
             }
+            Box{ CircularProgressIndicator() }
+
+            var progress by remember { mutableStateOf(0.0f) }
+            val scope = rememberCoroutineScope()
+
+            Column{
+                Text("Статус: $progress", fontSize = 22.sp)
+                OutlinedButton(
+                    modifier = Modifier.padding(20.dp),
+                    onClick = {
+                        scope.launch {
+                            while (progress < 1f) {
+                                progress += 0.1f
+                                delay(1000L)
+                            }
+                        }
+                    }
+                ) {
+                    Text("Запустить", fontSize = 22.sp)
+                }
+
+                LinearProgressIndicator(progress = progress)
+            }
+
+            var progress by remember { mutableStateOf(0.0f) }
+            val scope = rememberCoroutineScope()
+
+            Column{
+                OutlinedButton(
+                    modifier = Modifier.padding(20.dp),
+                    onClick = {
+                        scope.launch {
+                            while (progress < 1f) {
+                                progress += 0.1f
+                                delay(1000L)
+                            }
+                        }
+                    }
+                ) {
+                    Text("Запустить", fontSize = 22.sp)
+                }
+
+                LinearProgressIndicator(
+                    progress = progress,
+                    color = Color(0xFFD32F2F),
+                    backgroundColor = Color(0xFFEF9A9A)
+                )
+            }
+
+            Column{
+                LinearProgressIndicator(modifier = Modifier.padding(20.dp))
+            }
         }
-    }}
+        super.onCreate(savedInstanceState)
+    }
+}
