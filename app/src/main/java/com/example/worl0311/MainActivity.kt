c586b513ae2514396c1b9868f84f6afc6c246058
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 10:31:44 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -1,23 +1,38 @@
 package com.example.worl0311
-
+//buttonText
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.ui.Modifier
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.BorderStroke
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly)
+            val label = remember{mutableStateOf("Click")}
+            Button(onClick = {label.value = "Hello"}){
+                Text(label.value, fontSize = 25.sp)
+            }
+            Button(onClick = {},
+                colors = ButtonDefaults.buttonColors(contentColor = Color.Red)
+            )
             {
-                Box(modifier = Modifier.background(Color.Red).height(150.dp).width(100.dp))
-                Box(modifier = Modifier.background(Color.Yellow).height(150.dp).width(100.dp))
-                Box(modifier = Modifier.background(Color.Green).height(150.dp).width(100.dp))
+                Text("Click", fontSize = 25.sp)
+            }
+            Button(onClick = {},
+                colors = ButtonDefaults.buttonColors( contentColor = Color.Black),
+                border = BorderStroke(3.dp, Color.DarkGray)
+            )
+            {
+                Text("Click", fontSize = 30.sp)
             }
         }
     }
