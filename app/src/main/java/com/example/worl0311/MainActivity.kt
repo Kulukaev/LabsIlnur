93c6f368d52f0c81b18b04e6c3984716928485a6
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 10:25:52 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -3,28 +3,21 @@ package com.example.worl0311
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
-import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column{
-                val createMessage = {hour: Int ->
-                    if (hour > 18) {
-                        "Добрый вечер"
-                    } else if (hour > 10) {
-                        "Добрый день"
-                    } else {
-                        "Доброе утро"
-                    }
-                }
-
-                Text(text = createMessage(5), fontSize = 28.sp)
-                Text(text = createMessage(15), fontSize = 28.sp)
-                Text(text = createMessage(19), fontSize = 28.sp)
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly)
+            {
+                Box(modifier = Modifier.background(Color.Red).height(150.dp).width(100.dp))
+                Box(modifier = Modifier.background(Color.Yellow).height(150.dp).width(100.dp))
+                Box(modifier = Modifier.background(Color.Green).height(150.dp).width(100.dp))
             }
         }
     }
