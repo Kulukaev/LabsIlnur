1711c174f03dda491fdc5a27493449a3c33dedd9
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 10:36:58 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -2,53 +2,35 @@ package com.example.worl0311
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.*
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val openDialog = remember { mutableStateOf(false) }
-            Button(
-                onClick = { openDialog.value = true }
-            ) {
-                Text("Удалить", fontSize = 22.sp)
-            }
-            if (openDialog.value) {
-                AlertDialog(
-                    onDismissRequest = {
-                        openDialog.value = false
-                    },
-                    title = { Text(text = "Подтверждение действия") },
-                    text = { Text("Вы действительно хотите удалить выбранный элемент?") },
-                    confirmButton = {
-                        Row(
-                            modifier = Modifier.padding(all = 8.dp),
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            Button(
-                                modifier = Modifier.weight(1f),
-                                onClick = { openDialog.value = false }
-                            ) {
-                                Text("Удалить")
-                            }
-                            Button(
-                                modifier = Modifier.weight(1f),
-                                onClick = { openDialog.value = false }
-                            ) {
-                                Text("Отмена")
+            var progress by remember { mutableStateOf(0.0f) }
+            val scope = rememberCoroutineScope()
+
+            Column{
+                Text("Статус: $progress", fontSize = 22.sp)
+                OutlinedButton(
+                    onClick = {
+                        scope.launch {
+                            while (progress < 1f) {
+                                progress += 0.1f
+                                delay(1000L)
                             }
                         }
                     }
-                )
+                ) {
+                    Text("Запустить", fontSize = 22.sp)
+                }
+                CircularProgressIndicator(progress = progress)
             }
         }
     }
