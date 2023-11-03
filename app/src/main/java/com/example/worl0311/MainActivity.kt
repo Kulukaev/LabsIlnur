83f3472eb7a90a01118fa8c08d8f7f477b9a9ed6
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 10:21:55 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -3,22 +3,24 @@ package com.example.worl0311
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween )
-            {
-                Box(modifier = Modifier.background(Color.Red).fillMaxWidth().height(100.dp))
-                Box(modifier = Modifier.background(Color.Yellow).fillMaxWidth().height(100.dp))
-                Box(modifier = Modifier.background(Color.Green).fillMaxWidth().height(100.dp))
-            }
+            val message = remember{mutableStateOf("Hello Jetpack")}
+
+            Text(
+                text = message.value,
+                fontSize = 28.sp,
+                modifier = Modifier.clickable( onClick = { message.value = "Hello Metanit.com" })
+            )
         }
     }
 }
