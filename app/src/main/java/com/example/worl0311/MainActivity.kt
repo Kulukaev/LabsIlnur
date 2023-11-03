1678710855810d1168595a122b205875fb58f864
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 12:41:32 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -4,31 +4,45 @@ package com.example.worl0311
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.RadioButton
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scope = rememberCoroutineScope()
-            val snackbarHostState = remember { mutableStateOf(SnackbarHostState()) }
-            Button(
-                onClick = {
-                    scope.launch {
-                        snackbarHostState.value.showSnackbar("Hello")
+            val languages = listOf("Kotlin", "Java", "Javascript", "Rust")
+            val (selectedOption, onOptionSelected) = remember { mutableStateOf(languages[0]) }
+
+            Column(Modifier.selectableGroup()) {
+                languages.forEach { text ->
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(56.dp)
+                            .selectable(
+                                selected = (text == selectedOption),
+                                onClick = { onOptionSelected(text) }
+                            ),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = (text == selectedOption),
+                            onClick = null
+                        )
+                        Text( text = text, fontSize = 22.sp )
                     }
                 }
-            ) {
-                Text("Click", fontSize = 28.sp)
             }
-            SnackbarHost(snackbarHostState.value)
         }
     }
 }
