41b7f22a145b648deeebbe682600cdbeaa578e76
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 10:23:43 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -3,24 +3,29 @@ package com.example.worl0311
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.clickable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val message = remember{mutableStateOf("Hello Jetpack")}
+            Column{
+                val createMessage = {hour: Int ->
+                    if (hour > 18) {
+                        "Добрый вечер"
+                    } else if (hour > 10) {
+                        "Добрый день"
+                    } else {
+                        "Доброе утро"
+                    }
+                }
 
-            Text(
-                text = message.value,
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { message.value = "Hello Metanit.com" })
-            )
+                Text(text = createMessage(5), fontSize = 28.sp)
+                Text(text = createMessage(15), fontSize = 28.sp)
+                Text(text = createMessage(19), fontSize = 28.sp)
+            }
         }
     }
 }
