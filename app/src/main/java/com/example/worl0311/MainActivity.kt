4364bbf1b271f04fd17560c6c62686803e116a05
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 11:34:50 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -3,33 +3,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.*
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var sliderPosition by remember{mutableStateOf(0f)}
-            Column{
-                Text(text = "Текущее значение: ${sliderPosition}", fontSize = 22.sp)
-                Slider(
-                    value = sliderPosition,
-                    valueRange = 0f..10f,
-                    steps = 9,
-                    onValueChange = { sliderPosition = it },
-                    colors = SliderDefaults.colors(
-                        thumbColor = Color(0xFFB71C1C),
-                        activeTrackColor = Color(0xFFEF9A9A),
-                        inactiveTrackColor = Color(0xFFFFEBEE),
-                        inactiveTickColor = Color(0xFFEF9A9A),
-                        activeTickColor = Color(0xFFB71C1C)
+            val resources = LocalContext.current.resources
+            Column {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    fontSize = 28.sp
+                )
+                Text(
+                    text = stringResource(R.string.welcome),
+                    fontSize = 28.sp
+                )
+                Button(onClick={}){
+                    Text(
+                        text = stringResource(R.string.click_button),
+                        fontSize = 22.sp
                     )
+                }
+                Text(
+                    text = stringResource(R.string.user_data, "Tom", 37, "JetBrains"),
+                    fontSize = 28.sp
+                )
+                Text(
+                    text = stringResource(R.string.user_data, "Bob", 41, "Google"),
+                    fontSize = 28.sp
                 )
             }
         }
