b2da0a9eddbb7dc0bfe4c5d3999859d1386163da
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 11:40:50 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -1,38 +1,86 @@
 package com.example.worl0311
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.*
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("RememberReturnType", "UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TopAppBar(title = {
-            },
-                navigationIcon = {
-                    IconButton(onClick = {  }) {
-                        Icon(Icons.Filled.Menu, contentDescription = "Меню")
-                    }
-                    Row {
-                        Spacer(Modifier.weight(1f, true))
-                        Text("METANIT.COM", fontSize = 22.sp)
-                        IconButton(onClick = { }) {
-                            Icon(Icons.Filled.Info, contentDescription = "Информация о приложении")
+            val scaffoldState = remember {(SnackbarHostState())}
+            val scope = rememberCoroutineScope()
+            val selectedItem = remember{ mutableStateOf("")}
+            val items = listOf("Главная", "Контакты", "О приложении")
+            val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+            ModalNavigationDrawer(
+                drawerState = drawerState,
+                drawerContent = {
+                    ModalDrawerSheet {
+                        for (item in items) {
+                            Text(
+                                item,
+                                fontSize = 28.sp,
+                                modifier = Modifier.clickable {
+                                    selectedItem.value = item
+                                    scope.launch { drawerState.close() }
+                                }
+                            )
                         }
-                        IconButton(onClick = { }) {
-                            Icon(Icons.Filled.Search, contentDescription = "Поиск")
-                        }
                     }
-                })
+                },
+            ) {
+                Scaffold(
+                    floatingActionButton = {
+                        ExtendedFloatingActionButton(
+                            text = { Text("Show drawer") },
+                            icon = { Icon(Icons.Filled.Add, contentDescription = "") },
+                            onClick = {
+                                scope.launch {
+                                    drawerState.apply {
+                                        if (isClosed) open() else close()
+                                    }
+                                }
+                            }
+                        )
+                    }
+                ){
+                    Column{
+                        Button(onClick = {
+                            scope.launch{ drawerState.open() }
+                        }) {
+                            Text("Меню", fontSize = 28.sp)
+                        }
+                        Text("Выбран пункт: ${selectedItem.value}", fontSize = 28.sp)
+                    }
+                }
+            }
         }
-    }
-}
+    }}
