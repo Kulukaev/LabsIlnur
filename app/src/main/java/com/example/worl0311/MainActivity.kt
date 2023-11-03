4be9085549b5bf0f09fce682eeed9c59c420f153
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 11:02:39 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -2,36 +2,23 @@ package com.example.worl0311
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import androidx.compose.foundation.Image
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.res.imageResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var progress by remember { mutableStateOf(0.0f) }
-            val scope = rememberCoroutineScope()
-
-            Column{
-                Text("Статус: $progress", fontSize = 22.sp)
-                OutlinedButton(
-                    onClick = {
-                        scope.launch {
-                            while (progress < 1f) {
-                                progress += 0.1f
-                                delay(1000L)
-                            }
-                        }
-                    }
-                ) {
-                    Text("Запустить", fontSize = 22.sp)
-                }
-                CircularProgressIndicator(progress = progress)
-            }
+            Image(
+                painter = BitmapPainter(ImageBitmap.imageResource(R.drawable.forest)),
+                contentDescription = "Зимний лес"
+            )
+            Image(
+                bitmap = ImageBitmap.imageResource(R.drawable.forest),
+                contentDescription = "Зимний лес"
+            )
         }
     }
 }
