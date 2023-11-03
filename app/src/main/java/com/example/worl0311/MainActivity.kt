c386ce37909946c3c99afaf18fc98d71594dfece
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 12:42:32 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -1,48 +1,26 @@
 package com.example.worl0311
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.material3.RadioButton
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.Text
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.p6.ui.theme.MyText1
+
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val languages = listOf("Kotlin", "Java", "Javascript", "Rust")
-            val (selectedOption, onOptionSelected) = remember { mutableStateOf(languages[0]) }
-
-            Column(Modifier.selectableGroup()) {
-                languages.forEach { text ->
-                    Row(
-                        Modifier
-                            .fillMaxWidth()
-                            .height(56.dp)
-                            .selectable(
-                                selected = (text == selectedOption),
-                                onClick = { onOptionSelected(text) }
-                            ),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        RadioButton(
-                            selected = (text == selectedOption),
-                            onClick = null
-                        )
-                        Text( text = text, fontSize = 22.sp )
-                    }
-                }
-            }
+            val i = mutableStateOf(0)
+            MyText1(i, {i.value})
         }
     }
 }
