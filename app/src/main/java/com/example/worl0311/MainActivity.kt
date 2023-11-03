dccd3b36f67d583c08142395351a5b777ae1e550
Kulukaev <hnlmat1@yandex.ru>
2023-11-03 10:08:57 +0300

    6-2

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    //22
                     Greeting("Android")
                 }
             }
