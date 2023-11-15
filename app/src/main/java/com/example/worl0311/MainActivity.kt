7cf9eb343069f3ccb3753ed5b60c2f598a7c537b
Kulukaev <hnlmat1@yandex.ru>
2023-11-15 09:40:45 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -2,111 +2,111 @@ package com.example.worl0311
 
 
 
-import android.content.Context
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.example.worl0311.ui.theme.Worl0311Theme
-import org.json.JSONObject
 
 
-const val API_KEY = "e11d99d2101c56441ddadc90a43ca726"
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Worl0311Theme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+            var route = remember {
+                mutableStateOf(ScreenNames.LOGIN.name)
+            }
+            Column(modifier = Modifier.fillMaxSize()) {
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(16.dp)
                 ) {
-                    Greeting("Kazan", this)
+                    when (route.value) {
+                        ScreenNames.LOGIN.name -> LoginScreen()
+                        ScreenNames.GAME.name -> GameScreen()
+                        ScreenNames.MAP.name -> MapScreen()
+                        ScreenNames.RESULTS.name -> ResultsScreen()
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    NavButton(ScreenNames.LOGIN.name, route)
+                    NavButton(ScreenNames.GAME.name, route)
+                    NavButton( ScreenNames.MAP.name, route)
+                    NavButton(ScreenNames.RESULTS.name, route)
                 }
             }
         }
     }
-}
 
-
-@Composable
-fun Greeting(name: String, context: Context) {
-    val state = remember {
-        mutableStateOf("Unknown")
-    }
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxHeight(0.5f)
-                .fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "Temperature in $name: ${state.value} Cº")
+    @Composable
+    fun NavButton(text: String, f: MutableState<String>) {
+        Button(onClick = { f.value=text}) {
+            Text(text = text)
         }
-        Box(
-            modifier = Modifier
-                .fillMaxSize(),
-            contentAlignment = Alignment.BottomCenter
+    }
+    @Composable
+    fun LoginScreen() {
+        CardScreen(ScreenNames.LOGIN.toString())
+    }
+    @Composable
+    fun GameScreen() {
+        CardScreen(ScreenNames.GAME.toString())
+    }
+    @Composable
+    fun MapScreen() {
+        CardScreen(ScreenNames.MAP.toString())
+    }
+
+    @Composable
+    fun ResultsScreen() {
+        CardScreen(ScreenNames.RESULTS.toString())
+    }
+
+    @Composable
+    fun CardScreen(screenName: String) {
+        val cardPaddingModifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+        Card(
+            shape = CutCornerShape(30.dp),
+            modifier = cardPaddingModifier,
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
         ) {
-            Button(onClick = {
-                getData(name, context, state)
-            }, modifier = Modifier.fillMaxWidth().padding(5.dp)) {
-                Text(text = "Refresh")
+
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+
+                Text(
+                    screenName
+                )
             }
         }
-
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Worl0311Theme {
-
-    }
-}
-
-fun getData(name: String, context: Context, mState: MutableState<String>){
-    val url = "https://api.openweathermap.org/data/2.5/weather" +
-            "?appid=$API_KEY" +
-            "&q=$name" +
-            "&units=metric"
-    val queue = Volley.newRequestQueue(context)
-    val stringRequest = StringRequest(
-        Request.Method.GET,
-        url,
-        {
-                response->
-            val obj = JSONObject(response)
-
-            val main = obj.getJSONObject("main")
-            mState.value = main.getString("temp")
-        },
-        {
-        }
-    )
-    queue.add(stringRequest)
+enum class ScreenNames {
+    LOGIN, GAME, MAP, RESULTS
 }
