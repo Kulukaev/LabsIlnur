fb6aabb610e53c4b02888d5ccb2c029b2153d8ac
Kulukaev <hnlmat1@yandex.ru>
2023-11-15 10:28:34 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -5,25 +5,15 @@ package com.example.worl0311
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.example.worl0311.ui.theme.Minimal1
+import com.example.worl0311.botton_navigation.MainScreen
 import com.example.worl0311.ui.theme.Worl0311Theme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Worl0311Theme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ){
-                    Minimal1.MinimalNavigation()
-                }
+                MainScreen()
             }
         }
     }
