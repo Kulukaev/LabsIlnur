3f88f5ffcb1dadedc38fd6170566376402346e2e
Kulukaev <hnlmat1@yandex.ru>
2023-11-18 21:04:13 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -6,9 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -17,22 +17,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Screen1()
+            Screen2()
         }
     }
 }
 
-
 @Composable
-fun Screen1(myViewModel: MyViewModel1 = viewModel()) {
-    var i by myViewModel.i
+fun Screen2(myViewModel: MyViewModel3 = viewModel()) {
+    val i by myViewModel.i
     Text(
         text = "Клики: $i",
-        modifier = Modifier.clickable(onClick = { i++ } )
+        modifier = Modifier.clickable(onClick = myViewModel::increment)
     )
 }
 
+class MyViewModel2 : ViewModel() {
+    private val j = mutableStateOf(0)
+    val i: State<Int> = j
+    fun increment() {
+        j.value++
+    }
+}
 
-class MyViewModel1 : ViewModel() {
-    val i = mutableStateOf(0)
+class MyViewModel3 : ViewModel() {
+    var i = mutableStateOf(0)
+        private set
+    fun increment() {
+        i.value++
+    }
 }
