d384804a2f5a505ecd9b45484c0b5d62b07dfe04
Kulukaev <hnlmat1@yandex.ru>
2023-11-18 21:00:37 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -3,34 +3,36 @@ package com.example.worl0311
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 
 class MainActivity : ComponentActivity() {
-    val viewModel: MyViewModel1 by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
-        val i=viewModel.i
         super.onCreate(savedInstanceState)
         setContent {
-            Screen(i)
+            Screen0()
         }
     }
 }
 
 @Composable
-fun Screen(i: MutableState<Int>) {
+fun Screen0(myViewModel: MyViewModel = viewModel()) {
+    var i by remember { mutableStateOf(0) }
     Text(
-        text = "Клики: ${ i.value}",
-        modifier = Modifier.clickable(onClick = { i.value++ } )
+        text = "Клики: $i",
+        modifier = Modifier.clickable(onClick = { i++ } )
     )
 }
 
-class MyViewModel1 : ViewModel() {
+class MyViewModel : ViewModel() {
     val i = mutableStateOf(0)
 }
