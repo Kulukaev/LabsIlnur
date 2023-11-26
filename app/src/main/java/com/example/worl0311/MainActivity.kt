d83f7b23dfb6dc3caacec3afe0e977d6b50c12cf
Kulukaev <hnlmat1@yandex.ru>
2023-11-26 16:36:12 +0300

    6-3

--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val message = remember{mutableStateOf("Hello Jetpack")}
+            val message = remember{mutableStateOf("Hello Niger")}
 
             Text(
                 text = message.value,
