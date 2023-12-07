4955411b6e1b91571d04cdae871126adf9474a39
Kulukaev <hnlmat1@yandex.ru>
2023-12-07 20:09:46 +0300

    6-3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.github.javafaker:javafaker:1.0.2")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
--- app/src/main/java/com/example/worl0311/MainActivity.kt
+++ app/src/main/java/com/example/worl0311/MainActivity.kt
@@ -1,26 +1,52 @@
 package com.example.worl0311
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.clickable
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.javafaker.Faker
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val message = remember{mutableStateOf("Hello Jetpack")}
+            MyScreen()
+        }
+    }
+}
 
-            Text(
-                text = message.value,
-                fontSize = 28.sp,
-                modifier = Modifier.clickable( onClick = { message.value = "Hello Metanit.com" })
-            )
+@Composable
+fun CityItem(text: String) {
+    Text(text = text, fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(5.dp))
+}
+
+@SuppressLint("RememberReturnType")
+@Composable
+fun MyScreen() {
+    val faker = Faker()
+    val citiz = remember {
+        (1..50).map{ faker.address().city() }.toMutableStateList()
+    }
+    Column(modifier = Modifier.verticalScroll(ScrollState(0), true)) {
+        TextButton(onClick = { citiz.add(faker.address().city()) }) {
+            CityItem("+")
+        }
+        citiz.forEach {
+            key(it) { CityItem(it) }
         }
     }
 }
